de3e90561a329adb1bf5e0c4940ee140366b3440
priyanshu7273 <priyanshu7273@gmail.com>
2026-05-28 15:02:05 +0530

    Added database setup and User model

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 
+from extensions import db
+from models import User
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///placify.db"
+
+db.init_app(app)
 
 
 @app.route("/")
@@ -9,4 +15,6 @@ def hello():
 
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
